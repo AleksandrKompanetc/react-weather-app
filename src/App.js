a17ac16301cc9bee32e3452dad6d3432385f7a5e
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-07 23:26:36 +0300

    App component changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React, { useState } from 'react'
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&lat={lat}&lon={lon}&appid=150a49263a5b1ff973b07348fc7e8bda`
   return (
     <div className="App">
       
