2b07f65bf4ce99c12918536e5c84923c87a57156
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-07 23:47:39 +0300

    added changes in component App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,30 @@ function App() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&lat={lat}&lon={lon}&appid=150a49263a5b1ff973b07348fc7e8bda`
   return (
     <div className="App">
-      
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>30 C</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>35 C</p>
+          </div>
+          <div className='humidity'>
+            <p>20 %</p>
+          </div>
+          <div className='wind'>
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
