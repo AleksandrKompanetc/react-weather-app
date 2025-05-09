5ea734794931b02fae11c94350c66afb7d6cbda1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-09 23:57:28 +0300

    adding changes

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
             <p>Humidity</p>
           </div>
           <div className='wind'>
-            {data.wind ? <p className='bold'>{data.wind.speed}</p> : null
+            {data.wind ? <p className='bold'>{data.wind.speed}</p> : null}
             <p className='bold'>12 MPH</p>
             <p>Wind Speed</p>
           </div>
